927cd97fc95e4a1c21c019e4a3c7631b98e8daeb
fanialfi <fanialfirdaus@gmail.com>
2025-08-02 22:26:12 +0700

    data type - string

---
+++ data-types/primitive-types/string.js
@@ -0,0 +1,9 @@
+// string di javasctipt adalah kumpulan karakter yang idapit menggunakan tanda baca "" / '' / ``
+
+const nama = "Fani Alfirdaus";
+const name = "Fani Alfirdaus";
+const myName = `Fani Alfirdaus`;
+
+console.log(nama);
+console.log(name);
+console.log(myName);
