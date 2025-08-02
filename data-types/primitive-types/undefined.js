b7ace781bd7921297cb522902b631137eb5b980f
fanialfi <fanialfirdaus@gmail.com>
2025-08-02 23:02:00 +0700

    data type - undefined

---
+++ data-types/primitive-types/undefined.js
@@ -0,0 +1,5 @@
+// ketika mendefinisikan sebuah variabel tapi tidak langsung diisi sebuah value, maka secara default valuenya adalah undefined
+let name;
+console.log(name);
+name = "fani alfirdaus";
+console.log(name);
