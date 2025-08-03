ba7ef9587168ca0a662545d8bdbda51a1795a2f9
fanialfi <fanialfirdaus@gmail.com>
2025-08-03 14:16:39 +0700

    data type - boolean

---
+++ data-types/primitive-types/boolean.js
@@ -0,0 +1,7 @@
+// adalah sebuah tipe data simple yang hanya memiliki 2 value yaitu antara true dan falsee
+
+const bool = true;
+console.log(typeof bool, bool);
+
+const newBool = Boolean(10);
+console.log(typeof newBool, newBool);
