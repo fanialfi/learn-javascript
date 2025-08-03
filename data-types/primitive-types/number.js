7a3ec6763571b76987d1967c1f2b1c60b83ad255
fanialfi <fanialfirdaus@gmail.com>
2025-08-03 14:00:59 +0700

    data type - number

---
+++ data-types/primitive-types/number.js
@@ -0,0 +1,10 @@
+// tipe data number di javascript direpresentasikan sebagai floating-point
+// tipe data lain dapat diubah ke tipe data number dengan menggunakan `Number()` function
+
+const umur = 19.0;
+const usia = 19;
+
+console.log(umur == usia); // true
+
+const stringNumber = "19";
+console.log(Number(stringNumber));
